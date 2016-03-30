8761d491a72b4fb5e11b4830c101f895029187d4
Baso Ahmad Muflih Yunus <ahmadbaso97@gmail.com>
2016-03-30 10:20:13 +0700

    Added files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String args[]) {
+    
+        System.out.println("Baso Ahmad Muflih Yunus");
+        System.out.println("Masukkan Bilangan : ");
+        int number = new Scanner(System.in).nextInt();
+      
+        for(int i=1; i<=number; i++){
+            System.out.print(fibonacci(i) +" ");
+        }
+    } 
+ 
+    public static int fibonacci(int number){
+        if(number == 1 || number == 2){
+            return 1;
+        }
+        return fibonacci(number-1) + fibonacci(number -2); //tail recursion
+    }
+}
